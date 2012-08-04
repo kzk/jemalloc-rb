9a2814bc7313b755a2cd2f873f4d3c468a0628eb
Kazuki Ohta <kazuki.ohta@gmail.com>
2012-08-04 01:57:35 -0700

    add extconf.rb to build jemalloc

---
+++ ext/jemalloc/extconf.rb
@@ -0,0 +1,26 @@
+require 'mkmf'
+require File.expand_path('../../lib/je/version', File.dirname(__FILE__))
+
+$stdout.sync = true
+pkg = "jemalloc-3.0.0"
+
+cur_dir = Dir.pwd
+Dir.chdir File.dirname(__FILE__) do
+  # cleanup
+  puts `rm -fR #{pkg}`
+  puts `tar vjxf #{pkg}.tar.bz2`
+  Dir.chdir(pkg) do
+    # configure
+    puts `./configure`
+    # mkmf only allows *.c file on current dir
+    puts `cp src/*.c ../`
+    # zone.c is only for OS X Lion
+    puts `rm -fR ../zone.c`
+  end
+end
+Dir.chdir cur_dir
+
+include_dir= File.dirname(__FILE__) + "/#{pkg}/include/"
+$CFLAGS << %[ -std=gnu99 -Wall -pipe -g3 -fvisibility=hidden -O3 -funroll-loops -D_GNU_SOURCE -D_REENTRANT -I.. -I#{include_dir}]
+
+create_makefile('jemalloc')
