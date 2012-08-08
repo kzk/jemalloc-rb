8c2415a5549eb307d12ef4dbf821ccafdf363bf5
Kazuki Ohta <kazuki.ohta@gmail.com>
2012-08-07 19:01:23 -0700

    add support for MacOS X

--- ext/jemalloc/extconf.rb
+++ ext/jemalloc/extconf.rb
@@ -1,10 +1,10 @@
 require 'mkmf'
 require 'rbconfig'
-require File.expand_path('../../lib/je/version', File.dirname(__FILE__))
 
 $stdout.sync = true
 pkg = "jemalloc-3.0.0"
 
+# monfigure and copy sources to cur_dir
 cur_dir = Dir.pwd
 Dir.chdir File.dirname(__FILE__) do
   # cleanup
@@ -13,12 +13,12 @@ Dir.chdir File.dirname(__FILE__) do
   Dir.chdir(pkg) do
     # configure
     puts `./configure`
-    # mkmf only allows *.c file on current dir
-    puts `cp src/*.c ../`
     # zone.c is only for Mac OS X
     if RbConfig::CONFIG['target_vendor'] != "apple"
-      puts `rm -fR ../zone.c`
+      puts `rm -fR src/zone.c`
     end
+    # mkmf only allows *.c files on current dir
+    puts `cp src/*.c #{cur_dir}`
   end
 end
 Dir.chdir cur_dir
@@ -27,3 +27,13 @@ include_dir= File.dirname(__FILE__) + "/#{pkg}/include/"
 $CFLAGS << %[ -std=gnu99 -Wall -pipe -g3 -fvisibility=hidden -O3 -funroll-loops -D_GNU_SOURCE -D_REENTRANT -I.. -I#{include_dir}]
 
 create_makefile('jemalloc')
+
+# modify Makefile to create .dylib, instead of .bundle
+# NOTICE: Mac OS X only
+if RbConfig::CONFIG['target_vendor'] == "apple"
+  makefile = open('Makefile').read
+  makefile.gsub!(/-dynamic\ -bundle/, '-shared')
+  makefile.gsub!(/-flat_namespace/, '-dylib_install_name')
+  #,-dylib_install_name
+  open('Makefile', 'w'){ |f| f.write(makefile) }
+end
